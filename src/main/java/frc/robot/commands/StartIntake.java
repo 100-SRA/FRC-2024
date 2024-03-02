e3c3268a8bcf25ad3eb95e89e6506f7e3c5b382a
Malik Saafir <msaafir99@gmail.com>
2024-03-01 19:59:46 -0500

    finish StartIntake command constructor

--- src/main/java/frc/robot/commands/StartIntake.java
+++ src/main/java/frc/robot/commands/StartIntake.java
@@ -1,14 +1,12 @@
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ArmSubsystem;
 
 public class StartIntake extends Command{
-
     private final ArmSubsystem m_ArmSubsystem;
-    public StartIntake(){
-        
+    
+    public StartIntake(ArmSubsystem subsystem){
+        m_ArmSubsystem = subsystem;
     }
 }
